fc00c2a49d3eb20b624cc72cf05971ea8cf5e65c
sidhjaiswal <sidharth.jaiswal@students.fhnw.ch>
2025-02-20 14:47:39 +0100

    sizeof

---
+++ sizeOf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Size of char: %ld\n", sizeof(char));
+    printf("Size of int: %ld\n", sizeof(int));
+    printf("Size of double: %ld\n", sizeof(double));
+    printf("Size of float: %ld\n", sizeof(float));
+    printf("Size of long: %ld\n", sizeof(long));
+    return 0;
+}
